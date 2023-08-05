b84387d885af4a8010786b62ec3ec34d1fc1d518
csae8092 <peter.andorfer@oeaw.ac.at>
2023-08-05 15:13:26 +0200

    lets use the actual config file

--- fetch_manifest_urls.py
+++ fetch_manifest_urls.py
@@ -4,14 +4,14 @@ import time
 from tqdm import tqdm
 from transkribus_utils.transkribus_utils import ACDHTranskribusUtils
 
-from config import IIIF_COLLECTION_URL, IIIF_COL_DIR
+from config import IIIF_COLLECTION_URL, IIIF_COL_DIR, START_YEAR, END_YEAR
 
 tr_user = os.environ.get("TRANSKRIBUS_USER")
 tr_pw = os.environ.get("TRANSKRIBUS_PASSWORD")
 client = ACDHTranskribusUtils(user=tr_user, password=tr_pw)
 
 os.makedirs(IIIF_COL_DIR, exist_ok=True)
-for i in range(1751, 1761):
+for i in range(START_YEAR, END_YEAR):
     year = f"{i}"
     iiif_col_file = f"{os.path.join(IIIF_COL_DIR, str(year).lower())}.json"
     iiif_col_url = IIIF_COLLECTION_URL.format(year)
